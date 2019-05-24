55fb101e373bc33c74fad7874e2fc8f425f507b9
Jenkins <manangel@cisco.com>
2019-05-24 19:53:52 +0100

    update libparc formula to version 1.0-65~g7944543~b2

--- libparc.rb
+++ libparc.rb
@@ -3,8 +3,8 @@ class Libparc < Formula
   homepage "https://github.com/FDio/cicn/blob/cframework/master/libparc/README.md"
   url "https://github.com/FDio/cicn", :using=>:git, :branch=>"cframework/master"
 #@archive/cframework/master.zip"
-  version "1.0-65~g7944543~b1"
-  sha256 "c49e1cbb4c534ca72109a1d53ce83e683e5bc7b30026e6c5eeefb0b9ef7df333"
+  version "1.0-65~g7944543~b2"
+  
   depends_on "cmake" => :build
   depends_on "openssl@1.1"
   depends_on "libevent"
