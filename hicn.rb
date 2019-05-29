5d0ee85d096e11f1e51780769b58e647d39ba7b7
Jenkins <manangel@cisco.com>
2019-05-29 18:42:16 +0100

    update hicn formula to version 19.04-47-g088daca

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "Hicn stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.04-46-ge7149a6"
+  version "19.04-47-g088daca"
   
   depends_on "cmake" => :build
   depends_on "libparc"
