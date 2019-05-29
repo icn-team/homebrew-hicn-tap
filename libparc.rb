09dcfab24bb11d601c0036374dfffba0b4c6e5f9
Angelo Mantellini <manangel@cisco.com>
2019-05-30 00:47:58 +0200

    correct style libparc

--- libparc.rb
+++ libparc.rb
@@ -1,20 +1,19 @@
 class Libparc < Formula
-  desc "Libparc library"
-  homepage "https://github.com/FDio/cicn/blob/cframework/master/libparc/README.md"
-  url "https://github.com/FDio/cicn", :using=>:git, :branch=>"cframework/master"
-#@archive/cframework/master.zip"
-  version "1.0-65~g7944543~b3"
-  
-  depends_on "cmake" => :build
-  depends_on "openssl@1.1"
-  depends_on "libevent"
-
-  def install
-    system "cmake", "libparc", *std_cmake_args
-    system "make", "install", "-j"
-  end
-
-  test do
-    system "false"
-  end
+    desc "This library is part of hICN stack"
+    homepage "https://github.com/FDio/cicn/blob/cframework/master/libparc/README.md"
+    url "https://github.com/FDio/cicn", :using=>:git, :branch=>"cframework/master"
+    version "1.0-65~g7944543~b3"
+    
+    depends_on "cmake" => :build
+    depends_on "libevent"
+    depends_on "openssl@1.1"
+    
+    def install
+        system "cmake", "libparc", *std_cmake_args
+        system "make", "install"
+    end
+    
+    test do
+        system "#{bin}/parc-publickey", "-c", "keyfile.pkcs12", "12345", "test", "1024", "365"
+    end
 end
