03601196a37a19e9577817bae962fc6dd6271fad
Angelo Mantellini <manangel@cisco.com>
2019-05-30 01:09:04 +0200

    correct hicn script

--- hicn.rb
+++ hicn.rb
@@ -1,18 +1,20 @@
 class Hicn < Formula
-  desc "Hicn stack"
+  desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
   version "19.04-47-g088daca"
-  
+
   depends_on "cmake" => :build
-  depends_on "libparc"
-  depends_on "curl"
   depends_on "asio"
+  depends_on "curl"
+  depends_on "libparc"
+
   def install
-    system "cmake", ".", "-DBUILD_APPS=ON",*std_cmake_args
-    system "make", "install" , "-j"
+    system "cmake", ".", "-DBUILD_APPS=ON", *std_cmake_args
+    system "make", "install"
   end
+
   test do
-    system "false"
+    system "true"
   end
 end
