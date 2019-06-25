5795b300de1e860c006986e9352e7038a6563f41
Jenkins <manangel@cisco.com>
2019-06-25 14:26:44 -0700

    update hicn formula to version 19.04-61-g0415ae7

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.04-58-g7548d3f"
+  version "19.04-61-g0415ae7"
 
   depends_on "cmake" => :build
   depends_on "asio"
