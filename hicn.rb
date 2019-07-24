b89af372c153d3901bbc2ee0d4f4a9d2e0b785e8
Jenkins <manangel@cisco.com>
2019-07-24 12:11:38 -0700

    Merge "[HICN-243] Adding router plugin"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.04-77-gf12f9f6"
+  version "19.04-79-g932b7a2"
 
   depends_on "cmake" => :build
   depends_on "asio"
