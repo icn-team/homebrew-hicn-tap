b39bd7712f98062a2e4b883eebf495b27e2dea24
Jenkins <manangel@cisco.com>
2019-07-26 18:26:35 -0700

    [HICN-253] Mark cmake target for library as static or shared.

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.04-80-g7ceadff"
+  version "19.04-81-g05ca0aa"
 
   depends_on "cmake" => :build
   depends_on "asio"
