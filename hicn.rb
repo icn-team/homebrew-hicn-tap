9f02996b1e1d6c84004367a93dcb94d820ebbb33
Jenkins <manangel@cisco.com>
2019-08-02 09:26:42 -0700

    [HICN-255] the api vnet_get_sw_interface_safe is now vnet_get_sw_interface_or_null

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.04-82-g0a1c6b5"
+  version "19.04-83-g7b373ed"
 
   depends_on "cmake" => :build
   depends_on "asio"
