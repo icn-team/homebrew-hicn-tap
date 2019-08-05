261d9dc0d54d2f7597ffdd13dc85a0ba9909a835
Jenkins <manangel@cisco.com>
2019-08-05 09:11:55 -0700

    [HICN-257] facemgr for android

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.04-83-g7b373ed"
+  version "19.04-84-gaf6944a"
 
   depends_on "cmake" => :build
   depends_on "asio"
