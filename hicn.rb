dbcf366dcb792b30e4841ace10eac471d36ee01e
Jenkins <manangel@cisco.com>
2019-08-22 18:41:29 -0700

    [HICN-267] Fixed dependencies on hicn-apps and hicn-light packages

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-2-g7d14523"
+  version "19.08-3-g33eca8c"
 
   depends_on "cmake" => :build
   depends_on "asio"
