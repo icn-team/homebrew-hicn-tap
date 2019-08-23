59647775f7f9121017e1a9315411915f7e05787d
Jenkins <manangel@cisco.com>
2019-08-23 09:11:57 -0700

    Merge "HICN-266 fix strategy creation with policies"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-3-g33eca8c"
+  version "19.08-5-g3f70988"
 
   depends_on "cmake" => :build
   depends_on "asio"
