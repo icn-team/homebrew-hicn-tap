247dde528817444b41e53a3efba7ae21ae5ce35f
Jenkins <manangel@cisco.com>
2019-08-27 11:16:50 -0700

    Merge "[HICN-268] Forwarder crashes when "create listener hicn""

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-8-g69e841f"
+  version "19.08-10-g67f3d2e"
 
   depends_on "cmake" => :build
   depends_on "asio"
