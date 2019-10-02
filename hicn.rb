7b1b3058025ed86a4b3a37d66900c81167988721
Jenkins <manangel@cisco.com>
2019-10-02 06:11:02 -0700

    Merge "[HICN-286] Link libmemif with libhicntransport when libmemif found in the system."

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-19-g2db5743"
+  version "19.08-21-g761379d"
 
   depends_on "cmake" => :build
   depends_on "asio"
