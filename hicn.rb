5f0724d431dbca53346b99a7f14dd30c58791329
Jenkins <manangel@cisco.com>
2019-10-03 02:56:13 -0700

    [HICN-290] Added check if ASIO_STANDALONE already defined. Removed openssl find performed by hicn-light.

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-21-g761379d"
+  version "19.08-22-g523f12c"
 
   depends_on "cmake" => :build
   depends_on "asio"
