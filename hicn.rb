836957c2d7d1e98efa7409a4dee47a88c313b366
Jenkins <manangel@cisco.com>
2019-10-04 02:41:55 -0700

    [HICN-292] fix inflight interests counting

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-23-geaaff7f"
+  version "19.08-24-gceba475"
 
   depends_on "cmake" => :build
   depends_on "asio"
