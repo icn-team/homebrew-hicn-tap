c0c8ba23fd1a540e83269b341ffaa072379fdbbd
Jenkins <manangel@cisco.com>
2019-10-04 03:26:13 -0700

    [HICN-293] remove stats in hiperf in RTC mode

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-24-gceba475"
+  version "19.08-25-g821979b"
 
   depends_on "cmake" => :build
   depends_on "asio"
