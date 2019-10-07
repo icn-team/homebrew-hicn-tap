e53eff57eab82b9a30b4dd6a6142d529bacff0e8
Jenkins <manangel@cisco.com>
2019-10-07 05:26:08 -0700

    [HICN-289] update README

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-27-gdf63161"
+  version "19.08-28-g108c556"
 
   depends_on "cmake" => :build
   depends_on "asio"
