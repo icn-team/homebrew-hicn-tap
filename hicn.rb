157def04f68ac80246f6c8f0eb070fc89c602f3f
Jenkins <manangel@cisco.com>
2019-10-07 01:26:31 -0700

    [HICN-287] Adding hicn section to the startup.conf Checking if the size of the CS is greater than the number of vlib buffers available and size it accordingly. Removed unused parameters to set PIT lifetime

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-26-g85a791a"
+  version "19.08-27-gdf63161"
 
   depends_on "cmake" => :build
   depends_on "asio"
