6a4f224d0a505423f01cbca4cab24b752460532c
Jenkins <manangel@cisco.com>
2019-10-11 01:32:50 -0700

    Merge "[HICN-309] [network_framework] rules are now implemented by the core"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-28-g108c556"
+  version "19.08-51-g91304b8"
 
   depends_on "cmake" => :build
   depends_on "asio"
