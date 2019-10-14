4aff66fce921f91f3c5a197fd1836ec3d8268247
Jenkins <manangel@cisco.com>
2019-10-14 05:26:07 -0700

    Merge "[HICN-312] CMake file cleanup"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-53-g9a7c55f"
+  version "19.08-55-g1f25155"
 
   depends_on "cmake" => :build
   depends_on "asio"
