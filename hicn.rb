8041b761f243e312d49e06fd5df714f68b8a0181
Jenkins <manangel@cisco.com>
2019-10-16 08:41:09 -0700

    Merge "[HICN-334] remove LOG for no pending interests in portal"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-58-gd173417"
+  version "19.08-61-gd5512f6"
 
   depends_on "cmake" => :build
   depends_on "asio"
