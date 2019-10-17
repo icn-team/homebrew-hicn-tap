b2b87639c335f014a453061586aec276204aa06d
Jenkins <manangel@cisco.com>
2019-10-17 10:11:55 -0700

    [CICN-22] Impossible to push arm packages in packagecloud

--- libparc.rb
+++ libparc.rb
@@ -2,7 +2,7 @@ class Libparc < Formula
     desc "This library is part of hICN stack"
     homepage "https://github.com/FDio/cicn/blob/cframework/master/libparc/README.md"
     url "https://github.com/FDio/cicn", :using=>:git, :branch=>"cframework/master"
-    version "1.0-68~ge637823"
+    version "1.0-69~g8921515"
     
     depends_on "cmake" => :build
     depends_on "libevent"
