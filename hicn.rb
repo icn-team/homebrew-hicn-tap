8380e6f50b353786a4b46b60dc9bc626edc59927
Jenkins <manangel@cisco.com>
2019-10-18 05:26:33 -0700

    Merge "[HICN-337] correlty set timers for rtx in rtc consumer"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-65-gbadbec7"
+  version "19.08-68-g306b098"
 
   depends_on "cmake" => :build
   depends_on "asio"
