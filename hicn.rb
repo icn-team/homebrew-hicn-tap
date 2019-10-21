a8265952f44a70c330b3e044fccfc7772e249faf
Jenkins <manangel@cisco.com>
2019-10-21 07:43:07 -0700

    Merge "[HICN-320] HICN Light only configuring the first entry on FIB"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-71-g88706ad"
+  version "19.08-73-gfeb1b21"
 
   depends_on "cmake" => :build
   depends_on "asio"
