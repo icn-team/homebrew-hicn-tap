5b96d42a7e1352c1910ce23ebf0403d17bfca4fc
Jenkins <manangel@cisco.com>
2019-10-31 05:13:16 -0700

    [HICN-318] schedule rounds using timers in rtc conusmer

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-107-gfc6dfe9"
+  version "19.08-108-gaf6553e"
 
   depends_on "cmake" => :build
   depends_on "asio"
