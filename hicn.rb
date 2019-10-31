a054d10fb299312f8cb99e7dacab82e944006dba
Jenkins <manangel@cisco.com>
2019-10-31 08:43:10 -0700

    Merge "[HICN-354] Fixed bug on raaqm when reassemblying packets"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-111-g7b8858d"
+  version "19.08-113-gbf592d6"
 
   depends_on "cmake" => :build
   depends_on "asio"
