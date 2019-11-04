d185a206ab50b81a94786f99d76f6639ad221681
Jenkins <manangel@cisco.com>
2019-11-04 08:28:18 -0800

    [HICN-357] sysrepo plugin update

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-120-ged27687"
+  version "19.08-121-g6b7f4c3"
 
   depends_on "cmake" => :build
   depends_on "asio"
