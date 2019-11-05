a536f0a6d94e428711f2ea5dc7bbb688faff51f3
Jenkins <manangel@cisco.com>
2019-11-05 01:13:14 -0800

    [HICN-262] Fix binary api to prevent byteswapping of ip addresses in vapi

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-121-g6b7f4c3"
+  version "19.08-122-gccf813e"
 
   depends_on "cmake" => :build
   depends_on "asio"
