d37106574ed78577b17a69841b9ca4c97b9cbee4
Jenkins <manangel@cisco.com>
2019-11-15 08:13:33 -0800

    [HICN-386] Improve API error management in libhicnctrl

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-140-g56d4ce1"
+  version "19.08-141-g547acf3"
 
   depends_on "cmake" => :build
   depends_on "asio"
