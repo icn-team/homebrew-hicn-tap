3c7e728ef948747c4bf7252a8583e3f3f23f0549
Jenkins <manangel@cisco.com>
2019-11-17 01:58:37 -0800

    Merge "[HICN-395] Static face/route maintainance though face manager"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-142-g5d45b1b"
+  version "19.08-145-g2dcc257"
 
   depends_on "cmake" => :build
   depends_on "asio"
