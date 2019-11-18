80aba53f3bc40125a91a17e6a05fee446bf3faf2
Jenkins <manangel@cisco.com>
2019-11-18 02:58:38 -0800

    Merge "[HICN-225] Added generic binary api for handling faces"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-147-g7a8ed38"
+  version "19.08-149-gf159083"
 
   depends_on "cmake" => :build
   depends_on "asio"
