d864246033a97ebc7825115f32683cef6101ad67
Jenkins <manangel@cisco.com>
2019-11-20 13:43:31 -0800

    [HICN-404] double-free in facemgr (facemgr_list_facelets_json) + valgrind fixes

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-156-g1d1c54d"
+  version "19.08-157-g275b80b"
 
   depends_on "cmake" => :build
   depends_on "asio"
