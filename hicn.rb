34404239fab41dcc0aa46f2b99ab7a1df4b8d747
Jenkins <manangel@cisco.com>
2019-11-21 01:43:31 -0800

    Merge "[HICN-402] Limit in-flight interests for manifests"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-158-gae6f3b8"
+  version "19.08-160-g3638de1"
 
   depends_on "cmake" => :build
   depends_on "asio"
