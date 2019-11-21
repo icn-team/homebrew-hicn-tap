5963874e320e163c219c521a90491f7906c59701
Jenkins <manangel@cisco.com>
2019-11-20 16:44:40 -0800

    [HICN-379] Add face priority support in face manager

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-157-g275b80b"
+  version "19.08-158-gae6f3b8"
 
   depends_on "cmake" => :build
   depends_on "asio"
