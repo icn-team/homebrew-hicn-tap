b991ae335c47b7e215ce55506f8899e4c2fa703f
Jenkins <manangel@cisco.com>
2019-11-24 14:59:00 -0800

    Merge "[HICN-408] add a face manager interface for face priority control"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-171-g00702fa"
+  version "19.08-173-g02fef95"
 
   depends_on "cmake" => :build
   depends_on "asio"
