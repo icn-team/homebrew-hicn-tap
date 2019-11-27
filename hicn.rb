bb92eee650324353b4f14d58996aec9996ac5312
Jenkins <manangel@cisco.com>
2019-11-27 06:58:37 -0800

    [HICN-416] Fix error handling in facemgr netlink interface

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-180-g9f68d80"
+  version "19.08-181-gde378e3"
 
   depends_on "cmake" => :build
   depends_on "asio"
