3e66aa751262ab382f1c3e29214dffdd556e9132
Jenkins <manangel@cisco.com>
2019-12-02 07:58:33 -0800

    [HICN-426] Add isRunning API to consumer and producer socket.

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-190-g2d1b0a7"
+  version "19.08-191-g1c23af1"
 
   depends_on "cmake" => :build
   depends_on "asio"
