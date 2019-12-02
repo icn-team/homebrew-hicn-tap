7bea238e32f0caa354a85d9a625a0326252a31d3
Jenkins <manangel@cisco.com>
2019-12-02 04:58:36 -0800

    Merge "[HICN-425] RTC min window"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-187-g5822f2e"
+  version "19.08-189-g8bda9d9"
 
   depends_on "cmake" => :build
   depends_on "asio"
