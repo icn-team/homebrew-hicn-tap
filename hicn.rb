d2a23a0a30d55e257d8dece289353eba5d5f8b57
Jenkins <manangel@cisco.com>
2019-12-04 06:58:29 -0800

    [HICN-434] VAPI does not link to hicn sysrepo plugin

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-201-g92f586a"
+  version "19.08-202-gdafa145"
 
   depends_on "cmake" => :build
   depends_on "asio"
