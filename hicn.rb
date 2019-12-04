a17d091e8c7e95bf7e342d119547fd87342803e2
Jenkins <manangel@cisco.com>
2019-12-04 01:58:35 -0800

    Merge "to fix HICN-421 by adding cpack to libyang and sysrepo"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-193-gac2224a"
+  version "19.08-198-ga90afce"
 
   depends_on "cmake" => :build
   depends_on "asio"
