0a64abb8843ced2d6a7a6f126df170e25739698e
Jenkins <manangel@cisco.com>
2019-12-10 02:13:21 -0800

    Merge "[HICN-441] Fixed udp punting. Ip version of the punting (6 or 4) was set in the wrong way. Before enabling punting we were checking for the wrong table type (ip4 when it was 6 and vice versa)"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-207-gb0c1a5f"
+  version "19.08-209-ga40a23b"
 
   depends_on "cmake" => :build
   depends_on "asio"
