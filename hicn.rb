9099f784f5cfc38e1936b725751f687a3b317276
Jenkins <manangel@cisco.com>
2019-12-11 11:13:23 -0800

    [HICN-449] [HICN-430] Build sysrepo plugin. Update sysrepo and libyang dependencies for centos.

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-219-g0c9c060"
+  version "19.08-220-g631e07d"
 
   depends_on "cmake" => :build
   depends_on "asio"
