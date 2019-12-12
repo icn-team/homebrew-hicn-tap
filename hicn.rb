a531aef8e2bd44478cfd6e683baff64df58baca8
Jenkins <manangel@cisco.com>
2019-12-12 08:43:39 -0800

    Merge "[HICN-420] MAP-Me code refactoring & face manager changes in support of mobility"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-229-g02be1b9"
+  version "19.08-231-g63a3a23"
 
   depends_on "cmake" => :build
   depends_on "asio"
