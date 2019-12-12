33cf7b70ca5f1c605528f45f34855dc2bcb23661
Jenkins <manangel@cisco.com>
2019-12-12 04:43:21 -0800

    Merge "[HICN-449] Correct dependencies of DEB/RPM package for sysrepo."

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-223-g44ecc43"
+  version "19.08-225-gd4f976a"
 
   depends_on "cmake" => :build
   depends_on "asio"
