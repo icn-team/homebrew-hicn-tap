d3a9383280e428253ce6299025c31e907b72c4ec
Jenkins <manangel@cisco.com>
2019-12-12 09:13:19 -0800

    [HICN-451] libhicnctrl creates collisions in created connection names when two instances are started in the same second

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-231-g63a3a23"
+  version "19.08-232-g7795737"
 
   depends_on "cmake" => :build
   depends_on "asio"
