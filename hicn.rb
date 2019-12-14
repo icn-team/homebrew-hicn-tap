e054e4e1c63b0b43e0caf16e91bcfe0c1f3eb1dc
Jenkins <manangel@cisco.com>
2019-12-14 03:43:23 -0800

    [HICN-455] libhicnctrl incorrectly handles absence of forwarder in synchronous mode after it has connected

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-240-gd4aab68"
+  version "19.08-241-g1439c2b"
 
   depends_on "cmake" => :build
   depends_on "asio"
