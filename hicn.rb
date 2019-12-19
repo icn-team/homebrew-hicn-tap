ae1c52fcc4e0ea6bacc2f1b2a410bde8a4a0296a
Jenkins <manangel@cisco.com>
2019-12-19 07:28:21 -0800

    Merge "[HICN-464] swapping the initial buffer"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-246-g8112f87"
+  version "19.08-248-g724ca31"
 
   depends_on "cmake" => :build
   depends_on "asio"
