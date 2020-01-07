9cf235f04ad5a5e27689a719eb7a7ce721a47685
Jenkins <manangel@cisco.com>
2020-01-07 01:14:45 -0800

    Merge "[HICN-443] Adding test to hicn sysrepo plugin"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-249-g82f51e4"
+  version "19.08-251-gda609e6"
 
   depends_on "cmake" => :build
   depends_on "asio"
