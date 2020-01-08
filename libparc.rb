d58e5a1707e298f9ccfda2e2976df671a54cd02c
Jenkins <manangel@cisco.com>
2020-01-08 04:58:39 -0800

    [CICN-27] Fix invalid pointer dealloc

--- libparc.rb
+++ libparc.rb
@@ -2,7 +2,7 @@ class Libparc < Formula
     desc "This library is part of hICN stack"
     homepage "https://github.com/FDio/cicn/blob/cframework/master/libparc/README.md"
     url "https://github.com/FDio/cicn", :using=>:git, :branch=>"cframework/master"
-    version "1.0-72~gf5fe9c5"
+    version "1.0-73~ge0bc3a6"
     
     depends_on "cmake" => :build
     depends_on "libevent"
