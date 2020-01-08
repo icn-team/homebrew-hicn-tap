5148cdc289d89ccee2ff6578ea503b3d635b02c9
Jenkins <manangel@cisco.com>
2020-01-08 05:58:13 -0800

    Merge "[HICN-462] Adding probing protocol to hicn-plugin"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-251-gda609e6"
+  version "19.08-253-g61d8e9f"
 
   depends_on "cmake" => :build
   depends_on "asio"
