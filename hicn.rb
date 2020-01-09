2689f106afc4dae7f507d176f9b26d6bd855083a
Jenkins <manangel@cisco.com>
2020-01-09 08:58:11 -0800

    Merge "[HICN-469] execute hicn-light-control from remote"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-258-g3884838"
+  version "19.08-260-g7cb4fd8"
 
   depends_on "cmake" => :build
   depends_on "asio"
