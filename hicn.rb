4e24fb26f32a549582f3ff9167f4377ec8cc9d56
Jenkins <manangel@cisco.com>
2020-01-10 08:13:13 -0800

    Merge "[HICN-471] Fix wrong callback check in RAAQM"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-260-g7cb4fd8"
+  version "19.08-262-gee3014e"
 
   depends_on "cmake" => :build
   depends_on "asio"
