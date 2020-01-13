9f4983b8fb05d85318497d5dc685ff67e87fe383
Jenkins <manangel@cisco.com>
2020-01-13 08:28:05 -0800

    [HICN-473] Add getter functions for face manager rule configuration

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-263-g3f4acd6"
+  version "19.08-264-g63312d0"
 
   depends_on "cmake" => :build
   depends_on "asio"
