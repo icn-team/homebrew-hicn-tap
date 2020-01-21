f627b337d39019bdd52ce802a986c95d0cb75c55
Jenkins <manangel@cisco.com>
2020-01-21 16:43:11 +0100

    Merge "[HICN-477] Fixed strategy get ctx that could lead to a segfault"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-273-g43980f3"
+  version "19.08-275-g50bedb7"
 
   depends_on "cmake" => :build
   depends_on "asio"
