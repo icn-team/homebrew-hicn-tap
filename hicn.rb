d45eec2bc56e792310d9afe5933e8bcb8e2fa8bd
Jenkins <manangel@cisco.com>
2020-01-22 10:43:20 +0100

    Merge "[HICN-486] Writing face type in network order in the face_detail binary api message"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-277-gbf36608"
+  version "19.08-280-g6a371e0"
 
   depends_on "cmake" => :build
   depends_on "asio"
