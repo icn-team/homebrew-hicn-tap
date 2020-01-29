0b1bcf3e1f7763efaed301f7b2cfe3f992c686c4
Jenkins <manangel@cisco.com>
2020-01-29 17:13:11 +0100

    [HICN-491] Fixed missing inclusion of vapi_safe into the hicn-plugin deb/rpm  package

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "19.08-288-geebfbba"
+  version "19.08-289-gfa11d71"
 
   depends_on "cmake" => :build
   depends_on "asio"
