0344b4a394c7727edc8283208e0763679c1cc4ba
Jenkins <manangel@cisco.com>
2020-01-30 17:28:24 +0100

    [HICN-495 HICN-496] Compile only libmemif as part of extras. Upgrade libmemif to 20.01

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.01-2-gf1e21ec"
+  version "20.01-3-g682bcaa"
 
   depends_on "cmake" => :build
   depends_on "asio"
