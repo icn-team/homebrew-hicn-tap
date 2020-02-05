780e98ac2044d5c7d6520af68f92f736fd9fd940
Jenkins <manangel@cisco.com>
2020-02-05 17:43:20 +0100

    Merge "[HICN-503] correctly compute drop probability in RAAQM"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.01-16-g37fd13f"
+  version "20.01-21-g2f6e378"
 
   depends_on "cmake" => :build
   depends_on "asio"
