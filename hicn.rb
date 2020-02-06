1ecd19546ad0619a05ac75a21b0dc5630648f1ff
Jenkins <manangel@cisco.com>
2020-02-06 15:58:19 +0100

    [HICN-507] RTT estimation at packet reception

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.01-23-g8dbcb75"
+  version "20.01-24-g202c6bc"
 
   depends_on "cmake" => :build
   depends_on "asio"
