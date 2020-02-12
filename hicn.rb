db36057a192dd8571c6444b589b93ea6f8c834b9
Jenkins <manangel@cisco.com>
2020-02-12 15:58:19 +0100

    [HICN-513] fix uninitialized batching parameter in libtransport's rate estimation

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.01-25-g0e016eb"
+  version "20.01-26-gcea443f"
 
   depends_on "cmake" => :build
   depends_on "asio"
