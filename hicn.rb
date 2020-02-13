af5bf97484423547a33144c8ca74b90d88e0b804
Jenkins <manangel@cisco.com>
2020-02-13 21:43:22 +0100

    [HICN-508] [HICN-509] [HICN-506] Manifest rework

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.01-29-gf9243a2"
+  version "20.01-30-g3bce9bf"
 
   depends_on "cmake" => :build
   depends_on "asio"
