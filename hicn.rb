7b4d5a3f209a1882335c568d3f6120549c1ec65a
Jenkins <manangel@cisco.com>
2020-02-18 16:13:39 +0100

    [HICN-510] Adding collectd plugins to get telemetry from vpp and hicn-plugin

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.01-41-gfbe679d"
+  version "20.01-42-g4590ae6"
 
   depends_on "cmake" => :build
   depends_on "asio"
