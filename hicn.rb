d09c23d5596059d010e3e299fefa5ee6acddd82d
Jenkins <manangel@cisco.com>
2020-02-18 17:58:20 +0100

    [HICN-527] Add component for libhttpproxy.

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.01-42-g4590ae6"
+  version "20.01-43-ge432985"
 
   depends_on "cmake" => :build
   depends_on "asio"
