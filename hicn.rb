03c0e54cebf36edf0bded75d991b1aac85a53c67
Jenkins <manangel@cisco.com>
2020-02-19 16:58:33 +0100

    [HICN-530] Add support for chunked Transfer-Encoding in higet and http-proxy

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.01-45-g0710f1f"
+  version "20.01-46-g24acbd1"
 
   depends_on "cmake" => :build
   depends_on "asio"
