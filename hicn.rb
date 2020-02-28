1513ceda70d3fddfe4a96d9ae14378ba1d1c8ddc
Jenkins <manangel@cisco.com>
2020-02-28 12:14:42 +0100

    Merge "[HICN-539] Expose portal APIs to applications"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.01-50-g0e76694"
+  version "20.01-59-gee91ccd"
 
   depends_on "cmake" => :build
   depends_on "asio"
