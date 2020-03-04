fda315ddd9bf52c7a2e696171e0383995365df70
Jenkins <manangel@cisco.com>
2020-03-04 17:43:57 +0100

    [HICN-541] Change installation directory of collectd plugins

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.01-61-g248bfd5"
+  version "20.01-62-g2484112"
 
   depends_on "cmake" => :build
   depends_on "asio"
