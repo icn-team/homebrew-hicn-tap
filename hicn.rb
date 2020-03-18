87d9a36c084c80999d8926e34f69c94c785c5cb0
Jenkins <manangel@cisco.com>
2020-03-18 10:59:51 +0100

    Merge "[HICN-553] Fixed path to root cmake folder that contains the macros definitions"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.01-65-g01d76a6"
+  version "20.01-72-gb60a9ca"
 
   depends_on "cmake" => :build
   depends_on "asio"
