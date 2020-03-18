80f62265edfccdb37841a5662b2b80cad6937151
Jenkins <manangel@cisco.com>
2020-03-18 13:31:03 +0100

    [HICN-562] Add new options to VPP collectd plugins

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.01-72-gb60a9ca"
+  version "20.01-73-gcea861e"
 
   depends_on "cmake" => :build
   depends_on "asio"
