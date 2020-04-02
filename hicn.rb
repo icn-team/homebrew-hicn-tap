9376965d0b8650adf9dd26f0234e8606bd8303df
Jenkins <manangel@cisco.com>
2020-04-02 09:29:23 +0200

    [HICN-587] Fix RAAQM default config path

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.01-90-g59c4e43"
+  version "20.01-91-gd27d9b5"
 
   depends_on "cmake" => :build
   depends_on "asio"
