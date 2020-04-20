df5cc3516358616b492aa6a28e2888ff143bc676
Jenkins <manangel@cisco.com>
2020-04-20 13:14:09 +0200

    [HICN-597] Add API to mark packet as interest/data.

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.01-93-g512aa3a"
+  version "20.01-94-g43acd37"
 
   depends_on "cmake" => :build
   depends_on "asio"
