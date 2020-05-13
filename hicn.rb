2ab2aa20ffad97a425a8e21a640f2bd4def795aa
Jenkins <manangel@cisco.com>
2020-05-13 23:29:46 +0200

    Merge "[HICN-606] Added doxygen documentation"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.01-105-g43d0ecb"
+  version "20.01-108-g76c0e9a"
 
   depends_on "cmake" => :build
   depends_on "asio"
