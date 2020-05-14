bb8c8879f7b8f0136118c68192f854ef9f851de1
Jenkins <manangel@cisco.com>
2020-05-14 15:58:57 +0200

    [HICN-611] Remove unused fields on libhicnlibrary

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.01-108-g76c0e9a"
+  version "20.01-111-g67b8655"
 
   depends_on "cmake" => :build
   depends_on "asio"
