cc23fc4088dd34cf2e43d6218910d97be3fdb274
Jenkins <manangel@cisco.com>
2020-05-26 01:14:26 +0200

    [HICN-616] Fix compilation issue with asio 1.16.

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.01-113-ga27536f"
+  version "20.01-114-g0d89fdf"
 
   depends_on "cmake" => :build
   depends_on "asio"
