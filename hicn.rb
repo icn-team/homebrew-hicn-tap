fece8ffebe9b88e18b2c9bd153ea98a0fa701fb7
Jenkins <manangel@cisco.com>
2020-05-31 12:58:55 +0200

    [HICN-619] Compilation Error on Android

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.05-2-gf54ae4e"
+  version "20.05-3-gda86027"
 
   depends_on "cmake" => :build
   depends_on "asio"
