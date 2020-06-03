4b137efb5d1c23ea549c51e8485c2b3527ac3d8d
Jenkins <manangel@cisco.com>
2020-06-03 16:59:03 +0200

    Merge "[HICN-622] Add stop() functionality to http proxy."

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.05-4-g58ab29c"
+  version "20.05-8-gf3f4f8c"
 
   depends_on "cmake" => :build
   depends_on "asio"
