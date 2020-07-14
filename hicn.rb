7383d4459fa49bb28abe9d78c7c2b3c4a11ce6e3
Jenkins <manangel@cisco.com>
2020-07-15 00:05:40 +0200

    [HICN-632] Do not send packets larger more than 1400 bytes over RTC socket

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.05-19-gcb73ef0"
+  version "20.05-20-g90cb799"
 
   depends_on "cmake" => :build
   depends_on "asio"
