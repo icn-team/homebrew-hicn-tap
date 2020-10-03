8bfe6c18ad8a7ede4b755ed6577ead4488df46c4
Jenkins <manangel@cisco.com>
2020-10-03 11:46:59 -0700

    [HICN-643] Fix scripts for publishing release.

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.05-21-g5792a7d"
+  version "20.09-0-g9f0176d"
 
   depends_on "cmake" => :build
   depends_on "asio"
