7ad1a2d5c80811edb3c952c8811510ffb8af83ea
Jenkins <manangel@cisco.com>
2020-11-10 05:44:18 -0800

    [HICN-651] Offload checksum computation to VPP.

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.09-rc0-13-gc1de1ea"
+  version "20.09-rc0-14-gf87f19f"
 
   depends_on "cmake" => :build
   depends_on "asio"
