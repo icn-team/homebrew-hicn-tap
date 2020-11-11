676540a679b5f10190c774c403a9e178ba07bf22
Jenkins <manangel@cisco.com>
2020-11-11 04:58:54 -0800

    [HICN-653] Fix libmemif-{dev,devel} dependencies

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.09-rc0-15-g014ea2e"
+  version "20.09-rc0-16-gc0bd16d"
 
   depends_on "cmake" => :build
   depends_on "asio"
