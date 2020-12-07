31d8299ff4280f760162e8243f55497170bb3110
Jenkins <manangel@cisco.com>
2020-12-07 15:19:02 -0800

    [HICN-659] Fix udp-encap inconsistencies

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "$VERSION"
+  version 20.09-rc0-21-g190b321
 
   depends_on "cmake" => :build
   depends_on "asio"
