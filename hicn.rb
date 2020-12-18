b87d485849067f27053692848a392460bfdf662d
Jenkins <manangel@cisco.com>
2020-12-17 20:19:06 -0800

    Merge "[HICN-666] Fix libhicntransport-memif dependencies."

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.09-rc0-28-g8e278cb"
+  version "20.09-rc0-30-g5b64d41"
 
   depends_on "cmake" => :build
   depends_on "asio"
