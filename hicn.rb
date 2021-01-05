623b85d1a2ba81986aff41991a38f89235e77bbe
Jenkins <manangel@cisco.com>
2021-01-05 05:26:53 -0800

    [HICN-667] http proxy does not compile on android with ndk 22

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.09-rc0-32-g0042b03"
+  version "20.09-rc0-33-g82372f5"
 
   depends_on "cmake" => :build
   depends_on "asio"
