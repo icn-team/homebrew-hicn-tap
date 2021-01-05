f70bf76495113a5bab136832a2b139c0d3bb3a53
Jenkins <manangel@cisco.com>
2021-01-05 10:18:47 -0800

    [HICN-668] Fix various leaks across codebase

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.09-rc0-33-g82372f5"
+  version "20.09-rc0-34-gde6c35c"
 
   depends_on "cmake" => :build
   depends_on "asio"
