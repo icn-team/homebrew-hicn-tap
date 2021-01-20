e5b7ef0e750eba4ae738604b045f1d83049707a2
Jenkins <manangel@cisco.com>
2021-01-20 05:18:48 -0800

    [HICN-668] Fix leaks + double free

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.09-rc0-48-g3b1d7db"
+  version "20.09-rc0-49-g2d33dfd"
 
   depends_on "cmake" => :build
   depends_on "asio"
