81f524596aa2db60382e18865ee0d2e18a438508
Jenkins <manangel@cisco.com>
2021-02-10 15:31:42 -0800

    Merge "[HICN-679] Do not throw exception when receiving corrupted/not-hicn packet from network layer"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "20.09-rc0-57-gdb76bbc"
+  version "20.09-0-g71a5c39"
 
   depends_on "cmake" => :build
   depends_on "asio"
