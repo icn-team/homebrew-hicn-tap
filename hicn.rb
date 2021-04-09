f67e7aa1f60862dc7b1335daf41ed14c154e5eb9
Jenkins <manangel@cisco.com>
2021-04-09 15:00:14 +0200

    Merge "[HICN-696] packethicn: add support for native hicn and fix find wireshark for macOS"

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "21.01-rc0-12-gd6a155b"
+  version "21.01-rc0-14-g3e541d7"
 
   depends_on "cmake" => :build
   depends_on "asio"
