f6d8993a9f0af526036b061e6055dd1c4807c690
Jenkins <manangel@cisco.com>
2021-05-03 09:57:26 +0200

    [HICN-703] Update windows-sdk and hicn code

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "21.01-rc0-18-g39e15eb"
+  version "21.01-rc0-19-g07133ac"
 
   depends_on "cmake" => :build
   depends_on "asio"
