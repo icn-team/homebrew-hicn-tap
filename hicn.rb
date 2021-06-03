529c5a010b5d216a0993bedf99ba45b15e2b5906
Jenkins <manangel@cisco.com>
2021-06-03 04:22:00 -0700

    [HICN-705] correct package for windows

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn", :using=>:git, :branch=>"master"
-  version "21.01-rc0-24-g31eea8b"
+  version "21.01-rc0-30-g147ba39"
 
   depends_on "cmake" => :build
   depends_on "asio"
