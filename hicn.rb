6198c1afd26692cdf7e8e23957956374260f4a40
Angelo Mantellini <manangel@cisco.com>
2022-11-02 11:45:51 +0100

    hicn v0.0.5

--- hicn.rb
+++ hicn.rb
@@ -2,7 +2,7 @@ class Hicn < Formula
   desc "This formula contains the libs and the apps of hICN stack"
   homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/manang/hicn-sdk/releases/download/v0.0.1/binaries.zip"
-  version "v0.0.4"
+  version "v0.0.5"
   sha256 "8739c76e681f900923b900c9df0ef75cf421d39cabb54650c4b9ad19b6a76d85"
 
   depends_on "cmake" => :build
