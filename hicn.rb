0d1dec80a345ce6d8e622ca3e7f5ae3485ca4c82
Angelo Mantellini <manangel@cisco.com>
2022-11-03 10:22:19 +0100

    Update hicn.rb

--- hicn.rb
+++ hicn.rb
@@ -5,13 +5,15 @@ class Hicn < Formula
   version "v0.0.1"
   sha256 "a66f491cddad27f9b34511bcee96841d1d7fbb66f9c55d3238e937758dc8da2c"
 
-#   depends_on "cmake" => :build
   depends_on "openssl@1.1"
   depends_on "libevent"
   depends_on "asio"
   depends_on "libconfig"
 
   def install
-    bin.install "usr/bin/*" => "bin"
+    lib.install Dir['usr/lib/*']
+    bin.install Dir['usr/bin/*']
+    etc.install Dir['usr/etc/*']
+    include.install Dir['usr/include/*']
   end
 end
